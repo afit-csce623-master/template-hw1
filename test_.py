78e7398cda7b45c7b842398c1f8746f05a6f430e
wbwatkinson <blair.watkinson@me.com>
2021-04-11 23:58:18 -0400

    Update test_.py

--- test_.py
+++ test_.py
@@ -92,7 +92,7 @@ def test_step_6(tb):
     assert complete, 'STEP 6: not complete.' 
     
   try:
-    tb.ref('beta1min')
+    beta1min = tb.ref('beta1min')
   except:
     assert False, 'STEP 6: beta1min does not exist'
   assert abs(-0.158 - beta1min) < 0.01, 'Your beta1min value is not within 0.01 of the target beta1 minimum value. Try improving your beta1guess or reducing the distance between values in your beta1bracket array.' 
